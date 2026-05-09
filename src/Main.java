c0ab749ba1b2c8af4b2846823c0c061109ecd16a
Ilgner <ilgneramaral@hotmail.com>
2026-05-09 18:51:44 -0300

    treinandoVariaveis

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+String nome = "Maria";
+int idade = 25;
+double altura = 1.68;
+boolean estudante = true;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("É estudante: " + estudante);
+    }
+}
+/*      Objetivo é relembrar as variáveis e treinar sua execução
+        Nome: Maria
+        Idade: 25
+        Altura: 1.68
+        É estudante: true
+
+ */
