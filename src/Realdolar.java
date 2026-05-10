0c67d762b0175488dc2766b0608840db7030a601
Ilgner <ilgneramaral@hotmail.com>
2026-05-10 19:26:42 -0300

    treinandoVariaveis

---
+++ src/Realdolar.java
@@ -0,0 +1,11 @@
+public class Realdolar {
+    public static void main(String[] args) {
+
+        double valorReais = 451.50;
+        double taxaDeCambio = 5.25;
+
+        double dolar = valorReais/taxaDeCambio;
+
+        System.out.println ("O valor em dólares é: US$ " + dolar);
+    }
+}
